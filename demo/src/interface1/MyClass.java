fec0ec1a2292bd706a466855a4a08185b0e00667
Ankush <ankukoul07@gmail.com>
2018-10-03 21:09:52 -0700

    servletexample

--- demo/src/interface1/MyClass.java
+++ demo/src/interface1/MyClass.java
@@ -5,7 +5,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	Bank abc=new Bank_Axis();
-	Bank def=new Bank_Sbi();
 	Bank ijk=new Bank_ICICI();
 	
 	System.out.println(abc.getIntrestRate());
